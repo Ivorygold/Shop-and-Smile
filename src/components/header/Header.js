1a92fa22a37ba518e69f48f272b8cd1b9024262e
Udemba Godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-03-03 10:23:56 +0100

    create the nav elements

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
+import { BsCart4 } from "react-icons/bs";
 
 const logo = (
   <div className={styles.logo}>
@@ -12,10 +13,40 @@ const logo = (
   </div>
 );
 
+const cart = (
+  <span className={styles.cart}>
+    <Link to="/cart">
+      Cart
+      <BsCart4 size={20} />
+      <p>0</p>
+    </Link>
+  </span>
+);
+
 const Header = () => {
   return (
     <header>
-      <div className={styles.header}>{logo}</div>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles["header-right"]}>
+            <span className={styles.links}>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Order</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
