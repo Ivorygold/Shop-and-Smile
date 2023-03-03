4485b8aa49a42ef8d29112f4621407dce647aac0
Udemba Godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-03-03 08:12:03 +0100

    setting up the page with its initial Routing

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
